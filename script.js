5ebe7593f663ecc55fb6404fe65a508526e617d3
danendral <dlohanata@gmail.com>
2024-07-27 03:13:10 +0800

    Add function to generate computer choice

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log("Hello World!")
+
+// function to get computer choice randomly
+function getComputerChoice() {
+    const randomInt = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if (randomInt === 0) {
+        computerChoice = "Rock";
+    } else if (randomInt === 1) {
+        computerChoice = "Paper"; 
+    } else {
+        computerChoice = "Scissor";
+    }
+    console.log(computerChoice);
+}
+getComputerChoice();
+
+// function to get human choice, ensure it is a valid choice
+
+// declare human and compute scores
+
+// function playround to play 1 round, take parameters human and computer choice
+// assess the winner
+// update the score
+
+
+// function playgame, play 5 rounds
