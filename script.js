e4b9e6ac9828c63798036a9e23149a6312d8e81d
danendral <dlohanata@gmail.com>
2024-07-27 04:13:08 +0800

    Add function to check the winner and update score

--- script.js
+++ script.js
@@ -24,11 +24,41 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-// declare human and compute scores
+// initialise human and compute scores
+let humanScore = 0;
+let computerScore = 0; 
 
 // function playround to play 1 round, take parameters human and computer choice
-// assess the winner
-// update the score
+// assess the winner and update score
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "ROCK") {
+        if (computerChoice === "PAPER") {
+            computerScore += 1;
+        }
+        else if (computerChoice === "SCISSOR") {
+            humanScore += 1;
+        }
+    }
+    else if (humanChoice === "PAPER") {
+        if (computerChoice === "ROCK") {
+            humanScore += 1;
+        }
+        else if(computerChoice === "SCISSOR") {
+            computerScore += 1;
+        }
+    }
+    else {
+        if (computerChoice === "ROCK") {
+            computerScore += 1;
+        }
+        else if (computerChoice === "PAPER") {
+            humanChoice += 1;
+        }
+    }
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
+playRound(humanSelection, computerSelection);
 
 // function playgame, play 5 rounds
